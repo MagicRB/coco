11e93c6060144453cfc06367bbe4393552bb0084
magic_rb <richard@brazak.sk>
2025-01-16 16:16:32 +0100

    Fix shebangs and respect CC/CXX

--- framework/utils/compile-pass.sh
+++ framework/utils/compile-pass.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 LLVM_FLAGS=$(llvm-config-14 --cxxflags --libfiles all)
 SO_FLAGS=" -shared -fPIC -g -fsanitize=undefined -fno-sanitize-recover=all -Wno-missing-template-keyword"
 
 set -e
 
-g++ $LLVM_FLAGS $SO_FLAGS $1 -o $2
+$CXX $LLVM_FLAGS $SO_FLAGS $1 -o $2
